6565e314bb8d3d2b3d48ebc8dc002c809650bf30
Anuradha Sharma <anuradhasharma@Anuradhas-MacBook-Air.local>
2022-11-02 16:16:57 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-
+//for View-Engine
 app.set("view engine", "hbs");
-app.set('views', path.join(__dirname, '../views'));
-
+app.set("views", path.join(__dirname, "../views"));
+//set-Path
 const staticPath = path.join(__dirname, "../public");
 app.use(express.static(staticPath));
-
+//Routing
 app.get("/", (req, res) => {
   res.render("index.hbs");
 });
@@ -17,13 +17,11 @@ app.get("/about", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact.hbs");
 });
-
 app.get("*", (req, res) => {
   res.render("404", {
-    errorComponent: " oops couldn't find this page "
+    errorComponent: "OOPs page couldn't be found"
   });
 });
-
-app.listen(8000, () => {
-  console.log("listing the port at 8000");
+app.listen(8001, () => {
+  console.log("listing the port at 8001");
 });
